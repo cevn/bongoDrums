5b4bc8bfd0d6f85c5581fc36a39b8ed61fa73e28
Ahmed Khokar <amkhokar@gmail.com>
2018-09-09 16:38:57 -0700

    add keyup and keydown listeners to change border colors and play bongo sounds

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,101 @@
 var playBongo = function(squareNum) {
   if (squareNum === "0"){
-    var audio = document.getElementById("bongo1");
+    var audio = document.getElementById('bongo1');
+    audio.currentTime = 0;
     audio.play(); 
   }
   if (squareNum === "1") {
-    var audio = document.getElementById("bongo2");
+    var audio = document.getElementById('bongo2');
+    audio.currentTime = 0;
     audio.play();
   }
   if (squareNum === "2") {
-    var audio = document.getElementById("bongo3");
-    audio.play();
+    var audio = document.getElementById('bongo3');
+    audio.currentTime = 0;
+    audio.play(); 
+    
   }
   if (squareNum === "3") {
-    var audio = document.getElementById("bongo4");
+    var audio = document.getElementById('bongo4');
+    audio.currentTime = 0;
     audio.play();
   }
-  else {
-    var audio = document.getElementById("bongo5");
+  if(squareNum === "4") {
+    var audio = document.getElementById('bongo5');
+    audio.currentTime = 0;
     audio.play(); 
   }
+
+
 }
 $(document).ready(function () {
+
+  document.addEventListener('keydown', function (event) {
+    if (event.which == 49) {
+      var audio = document.getElementById('bongo1')
+      audio.currentTime = 0;
+      audio.play();
+      var borderElement = document.getElementById('0');
+      borderElement.style.border = "3px solid black";
+    }
+    if (event.which == 50) {
+
+      var audio = document.getElementById('bongo2')
+      audio.currentTime = 0;
+      audio.play();
+      var borderElement = document.getElementById('1');
+      borderElement.style.border = "3px solid black";
+      
+    }
+    if (event.which == 51) {
+
+      var audio = document.getElementById('bongo3')
+      audio.currentTime = 0;
+      audio.play();
+      var borderElement = document.getElementById('2');
+      borderElement.style.border = "3px solid black";
+    }
+    if (event.which == 52) {
+
+      var audio = document.getElementById('bongo4')
+      audio.currentTime = 0;
+      audio.play();
+      var borderElement = document.getElementById('3');
+      borderElement.style.border = "3px solid black";
+    }
+    if (event.which == 53) {
+
+      var audio = document.getElementById('bongo5')
+      audio.currentTime = 0;
+      audio.play();
+      var borderElement = document.getElementById('4');
+      borderElement.style.border = "3px solid black";
+    }
+  });
+
+  document.addEventListener('keyup', function (event) {
+    if (event.which == 49) {
+      var borderElement = document.getElementById('0');
+      borderElement.style.border = "3px solid white";
+    }
+    if (event.which == 50) {
+      var borderElement = document.getElementById('1');
+      borderElement.style.border = "3px solid white";
+    }
+    if (event.which == 51) {
+      var borderElement = document.getElementById('2');
+      borderElement.style.border = "3px solid white";
+    }
+    if (event.which == 52) {
+      var borderElement = document.getElementById('3');
+      borderElement.style.border = "3px solid white";
+    }
+    if (event.which == 53) {
+      var borderElement = document.getElementById('4');
+      
+      borderElement.style.border = "3px solid white";
+    }
+  });
   $(".game-square").click(function () {
     var squareId = $(this).attr('id');
     playBongo(squareId);
