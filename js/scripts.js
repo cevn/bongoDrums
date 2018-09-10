9530b1ac91d5a527c0b9ca0364e53da9e86d39a7
Sameer Dhar <sdhar@protonmail.com>
2018-09-10 00:59:17 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -74,48 +74,8 @@ $(document).ready(function () {
   });
 
   document.addEventListener('keyup', function (event) {
-    if (event.which == 49) {
-      var borderElement = document.getElementById('0');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 50) {
-      var borderElement = document.getElementById('1');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 51) {
-      var borderElement = document.getElementById('2');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 52) {
-      var borderElement = document.getElementById('3');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 53) {
-      var borderElement = document.getElementById('4');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 54) {
-      var borderElement = document.getElementById('5');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 55) {
-      var borderElement = document.getElementById('6');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 56) {
-      var borderElement = document.getElementById('7');
-      borderElement.style.border = "3px solid white";
-      borderElement.style.color = "white";
-    }
-    if (event.which == 57) {
-      var borderElement = document.getElementById('8');
+    if (event.which >= 49 && event.which <= 57) {
+      var borderElement = document.getElementById(event.which-49);
       borderElement.style.border = "3px solid white";
       borderElement.style.color = "white";
     }
